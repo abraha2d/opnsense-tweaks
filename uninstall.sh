269dd0911fa49a366ffb073d9b55b9c61381ae49
Kevin Abraham <abraha2d@users.noreply.github.com>
2022-02-11 01:07:33 -0500

    Make sure the uninstall script actually uninstalls everything

--- uninstall.sh
+++ uninstall.sh
@@ -1,5 +1,6 @@
 killall dhcpcd
 ( cd /usr/ports/net/dhcpcd && make deinstall )
 
+rm /usr/local/etc/dhcpcd.conf
 rm /usr/local/etc/rc.syshook.d/carp/10-wandhcp
 rm /usr/local/libexec/dhcpcd-hooks/10-wancarp
