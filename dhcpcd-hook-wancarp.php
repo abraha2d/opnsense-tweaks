9e00aa26952395880062ddaf5704c557396a6ca5
Kevin Abraham <abraha2d@users.noreply.github.com>
2024-04-13 22:40:36 -0400

    Update to work with gateway MVC, improve logging

--- dhcpcd-hook-wancarp.php
+++ dhcpcd-hook-wancarp.php
@@ -17,33 +17,50 @@ $new_routers = getenv('new_routers');
 
 # Die if we don't have the necessary info
 if (empty($interface) || empty($new_ip_address) || empty($new_subnet_cidr) || empty($new_routers)) {
+  log_error("Did not get one of '$interface', '$new_ip_address', '$new_subnet_cidr', '$new_routers'");
   exit(1);
 }
+log_error("$interface DHCP: $new_ip_address/$new_subnet_cidr, gateway $new_routers");
 
 # Translate interface name
 $ifname = convert_real_interface_to_friendly_interface_name($interface);
+log_error("Mapped $interface to $ifname");
 
 # Find existing CARP config
 $a_vip = &config_read_array('virtualip', 'vip');
 $vid = array_search($ifname, array_column($a_vip, 'interface'));
+if ($vid === false) {
+  log_error("Did not find CARP for $ifname");
+  exit(1);
+}
+
 log_error("Found $ifname CARP at index $vid");
 $subnet = $a_vip[$vid]['subnet'];
 $subnet_bits = $a_vip[$vid]['subnet_bits'];
+log_error("$ifname CARP: $subnet/$subnet_bits");
 
 # Find existing gateway config
-$a_gateway_item = &config_read_array('gateways', 'gateway_item');
+$gateways = new \OPNsense\Routing\Gateways();
+$a_gateway_item = iterator_to_array($gateways->gatewayIterator());
 $gid = array_search($ifname, array_column($a_gateway_item, 'interface'));
+if ($gid === false) {
+  log_error("Did not find gateway for $ifname");
+  exit(1);
+}
+
 log_error("Found $ifname gateway at index $gid");
 $gateway = $a_gateway_item[$gid]['gateway'];
+log_error("$ifname gateway: $gateway");
 
 # Don't do anything if the new lease matches the existing config
 if ($subnet == $new_ip_address && $subnet_bits == $new_subnet_cidr && $gateway == $new_routers) {
+  log_error("Nothing to update for $ifname");
   exit(0);
 }
 
 # Update existing gateway
 log_error("Updating $ifname gateway from $gateway to $new_routers");
-$a_gateway_item[$gid]['gateway'] = $new_routers;
+$gateways->createOrUpdateGateway(['gateway' => $new_routers], $a_gateway_item[$gid]['uuid']);
 
 # Update existing NAT outbound rule
 $a_out = &config_read_array('nat', 'outbound', 'rule');
@@ -61,6 +78,9 @@ interface_vip_bring_down($a_vip[$vid]);
 log_error("Updating $ifname CARP IP address from $subnet/$subnet_bits to $new_ip_address/$new_subnet_cidr");
 $a_vip[$vid]['subnet'] = $new_ip_address;
 $a_vip[$vid]['subnet_bits'] = $new_subnet_cidr;
+
+# Serialize and write config
+$config = OPNsense\Core\Config::getInstance()->toArray(listtags());
 write_config();
 
 # Re-configure the CARP virtual IP
